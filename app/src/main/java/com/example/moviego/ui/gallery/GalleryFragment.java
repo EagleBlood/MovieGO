0d356bae79ab993e354aeb9d9b8c76ab9c9d1381
Dawid Nosal <35221@student.pwsztar.edu.pl>
2023-04-02 15:46:19 +0200

    Theme color changed, user credentials fragment added

--- app/src/main/java/com/example/moviego/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/moviego/ui/gallery/GalleryFragment.java
@@ -24,8 +24,7 @@ public class GalleryFragment extends Fragment {
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
     }
 
