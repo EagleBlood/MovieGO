fbd4d3f1aba168f51895ed3c56fadf5a379858a7
Dawid Nosal <35221@student.pwsztar.edu.pl>
2023-06-01 12:09:49 +0200

    LoginCredentials added, first connection attempt

--- app/src/main/java/com/example/moviego/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/moviego/ui/login/LoginFragment.java
@@ -18,9 +18,16 @@ import com.example.moviego.DrawerAndBottomNavActivity;
 import com.example.moviego.R;
 import com.example.moviego.databinding.FragmentLoginBinding;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class LoginFragment extends Fragment {
 
     private FragmentLoginBinding binding;
+    private TextView inputUser;
+    private TextView inputPass;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -51,10 +58,35 @@ public class LoginFragment extends Fragment {
             fragmentTransaction.commit();
         });
 
+        inputUser = root.findViewById(R.id.login_userInput);
+        inputPass = root.findViewById(R.id.login_passInput);
         Button logIn = root.findViewById(R.id.login_logIn);
-        logIn.setOnClickListener(v -> ((DrawerAndBottomNavActivity) getActivity()).reloadApp());
+        logIn.setOnClickListener(v -> {
+            try {
+                connectJDBC();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
 
         return root;
+    }
+
+    private void connectJDBC() throws IOException {
+        String dbUrl = LoginCredentials.getDbUrl();
+        String username = String.valueOf(inputUser.getText());
+        String password = String.valueOf(inputPass.getText());
+        LoginCredentials.setCredentials(username, password);
+
+        try {
+            Connection connection = DriverManager.getConnection(dbUrl, username, password);
+            System.out.println("Success");
+            ((DrawerAndBottomNavActivity) getActivity()).reloadApp();
+            connection.close(); // Close the connection when you're done
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
