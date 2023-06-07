3527c5d45afc6c8df6394fa4c41637632b1868d1
wiktor10 <wuszko1@gmail.com>
2023-06-07 10:37:03 +0200

    retrofit connection v2

--- app/src/main/java/com/example/moviego/retrofit/DataAPI.java
+++ app/src/main/java/com/example/moviego/retrofit/DataAPI.java
@@ -1,4 +1,27 @@
 package com.example.moviego.retrofit;
 
-public class DataAPI {
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface DataAPI {
+
+//    @GET("shoutbox/messages?last=12")
+//    Call<List<ShoutboxData>> getShoutboxData();
+//
+//    @POST("shoutbox/message")
+//    Call<DataApiResponse> postJson(@Body DataApiResponse dataApiResponse);
+//
+//    @PUT("shoutbox/message/{id}")
+//    Call<DataApiResponse> editPost(@Path("id") String id, @Body DataApiResponse dataApiResponse);
+//
+//    @DELETE("shoutbox/message/{id}")
+//    Call<ShoutboxData> deleteItem(@Path("id") String id);
+
 }
--- app/src/main/java/com/example/moviego/retrofitFunction.java
+++ app/src/main/java/com/example/moviego/retrofitFunction.java
@@ -1,21 +1,58 @@
 package com.example.moviego;
 
-import com.example.moviego.retrofit.DataAPI;
-
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class retrofitFunction {
+public class retrofitFunction<T> {
 
-    private DataAPI dataAPI;
+    private T dataAPI;
 
-    private void retrofitFun(String endpoint){
+    public void retrofitClient(Class<T> apiClass) {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://localhost:8080/"+endpoint)
+                .baseUrl("http://localhost:8080/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        dataAPI = retrofit.create(DataAPI.class);
+        dataAPI = retrofit.create(apiClass);
     }
 
+    public void getData(Call<T> call, final Callback<T> callback) {
+        call.enqueue(new Callback<T>() {
+            @Override
+            public void onResponse(Call<T> call, Response<T> response) {
+                if (response.isSuccessful()) {
+                    callback.onResponse(call, response);
+                } else {
+                    callback.onFailure(call, new Throwable("Response unsuccessful. Status code: " + response.code()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<T> call, Throwable t) {
+                callback.onFailure(call, t);
+            }
+        });
+    }
+
+    public void sendData(Call<T> call, final Callback<T> callback) {
+        call.enqueue(new Callback<T>() {
+            @Override
+            public void onResponse(Call<T> call, Response<T> response) {
+                if (response.isSuccessful()) {
+                    callback.onResponse(call, response);
+                } else {
+                    callback.onFailure(call, new Throwable("Response unsuccessful. Status code: " + response.code()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<T> call, Throwable t) {
+                callback.onFailure(call, t);
+            }
+        });
+    }
 }
+
